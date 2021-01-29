c842f27e0f2f22823517aae8cd27bf773c639c20
chockty <75976150+chockty@users.noreply.github.com>
2021-01-29 14:04:13 +0900

    修正

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,8 +1,10 @@
 $(function(){
-  console.log("OK")
   $('.multiple-items').slick({
-    infinite: true,
+    autoplay:true,
+    autoplaySpeed:5000,
     dots:true,
+    // infinite: true,
+    // dots:true,
     slidesToShow: 3,
     slidesToScroll: 3
   });
